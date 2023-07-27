ef507ad7f1c153570e5a7188683f345748c2732f
krish4uu <prajapatikashyap97@gmail.com>
2023-07-26 20:39:03 -0400

    Updated router

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import Projects from "./components/Projects/Projects";
 import Certificates from "./components/certificates/Certificates";
 import Footer from "./components/Footer";
 import Resume from "./components/Resume/Resume";
-import { HashRouter as Router, Route, Routes } from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
 import "./style.css";
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
@@ -26,22 +26,19 @@ function App() {
   return (
     <>
       <Particle />
-      <Router>
-        <Preloader load={load} />
-        <div className="App" id={load ? "no-scroll" : "scroll"}>
-          <Navbar />
-          <ScrollToTop />
-          <Routes>
-            <Route path="/" exact element={<Home />} />
-            <Route path="/project" element={<Projects />} />
-            <Route path="/about" element={<About />} />
-            <Route path="/resume" element={<Resume />} />
-            <Route path="/certificates" element={<Certificates />} />
-          </Routes>
-
-          <Footer />
-        </div>
-      </Router>
+      <Preloader load={load} />
+      <div className="App" id={load ? "no-scroll" : "scroll"}>
+        <Navbar />
+        <ScrollToTop />
+        <Routes>
+          <Route path="/" exact element={<Home />} />
+          <Route path="/project" element={<Projects />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/resume" element={<Resume />} />
+          <Route path="/certificates" element={<Certificates />} />
+        </Routes>
+        <Footer />
+      </div>
     </>
   );
 }
